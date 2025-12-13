bf71510a60702ac1a33ab558751d5d824db3926a
Natalie Chan <natkuma@outlook.com>
2025-12-13 04:58:01 -0500

    Create landing login page, need functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import RestaurantList from "./features/restaurants/RestaurantList";
 import WineList from "./features/wines/WineList";
 import { Route, Routes } from "react-router-dom";
 import InStock from "./features/inventories/InStock";
+import Landing from "./landing";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<RestaurantList />} />
         <Route path="wines/:id" element={<WineList />} />
         <Route path="inventories/:wineId" element={<InStock />} />
