5329cc0dad76d0fb6f5a77ff7c48d9e4fe7fe23f
Natalie Chan <natkuma@outlook.com>
2025-12-15 03:02:38 -0500

    add gitignore and .env file

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,11 +94,11 @@ WSGI_APPLICATION = "main.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wine_not',
-        'USER': 'nataliechan',
-        'PASSWORD': 'jebon171',
-        'HOST': 'localhost',  
-        'PORT': '5432',  
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
